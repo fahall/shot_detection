d2d46a2cb1f751eea1e5cc03fa7d5ea23336f097
Alex Hall <alexhall@eecs.berkeley.edu>
2017-08-15 02:09:00 -0700

    updating readme and requirements.txt

---
+++ lib/config.py
@@ -0,0 +1,37 @@
+import sys
+
+
+def make_std_dev_thresh_func(multiplier):
+    def std_dev_thresh_func(x, a):
+        std_dev = np.std(x)
+        return multiplier * std_dev
+    return std_dev_thresh_func
+
+
+OUTPUT_TXT_FNAME = 'shots.txt'
+OUTPUT_CSV_FNAME = 'shots.csv'
+SOURCE_FRAME_DIR = 'temp_frames'
+
+FRAME_FNAME_FORMAT = '%06d.jpg'
+FILE_TYPES = ['.mp4', '.m4v', '.mkv']
+IGNORES = ['movie_title_to_ignore']
+
+THRESH_MULTIPLIER = 0.5
+
+WAIT_TIME = 0.5
+DEFAULT_NUM_SEGMENTS = 10
+FRAME_CHUNK_SIZE = 10000
+
+CLEANUP = True  # Delete images when finished
+DECOMPOSE = True  # Convert movie file into images
+
+
+######## TRAIN & TEST ONLY ########
+RESOURCES_PATH = "/Users/alexhall/Documents/filmGrammar/deepak_ground_truth/frames/"
+GROUND_DIR = '/Users/alexhall/Documents/filmGrammar/deepak_ground_truth/classification_shot/ground_truth_0_index'
+TEST_PATH = '/Users/alexhall/Documents/filmGrammar/deepak_ground_truth/test_dataset'
+LOCAL_MAXIMA_THRESH = 0.1
+VAL = 0.5
+
+
+def THRESH_FUNC(x, a): return VAL
